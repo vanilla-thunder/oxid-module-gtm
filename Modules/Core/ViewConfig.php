a25be1069cbfc9a6f88c9fac8e8182aa0434bfe3
MaxBUhe <mb@shopmodule.com>
2022-12-01 13:30:09 +0100

    rework code to actually fit 6.0.x && 6.1x

--- Modules/Core/ViewConfig.php
+++ Modules/Core/ViewConfig.php
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>
  */
 
-namespace D3\GoogleAnalytics4\Modules\Core;
+namespace VanillaThunder\GoogleTagManager\Application\Extend;
 
 use OxidEsales\Eshop\Application\Controller\FrontendController;
 use OxidEsales\Eshop\Core\Registry;
