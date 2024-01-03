ed5fcc60117a1ddfba4d55ad8e2baf28780f131c
MaxBUhe <mb@shopmodule.com>
2024-01-03 13:48:43 +0100

    bump changelog && version to 2.13.1

--- metadata.php
+++ metadata.php
@@ -26,7 +26,7 @@ $aModule          = [
                       Die Entwicklung basiert auf einem Fork von Marat Bedoev - <a href='https://github.com/vanilla-thunder/oxid-module-gtm'>Github-Link</a>
                       ",
     'thumbnail'   => 'thumbnail.png',
-    'version'     => '2.13.0',
+    'version'     => '2.13.1',
     'author'      => 'Data Development (Inh.: Thomas Dartsch)',
     'email'       => 'support@shopmodule.com',
     'url'         => 'https://www.oxidmodule.com/',
